f094ed9432652a8a74395138586a7187e75e13cf
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-04-12 09:42:18 +0200

    psw gen v1

---
+++ codice.js
@@ -0,0 +1,7 @@
+alert ( `questo è un generatore di password` )
+let annoCorrente = `2023`
+let nome = prompt( `inserisci il tuo nome` )
+let cognome = prompt( `inserisci il tuo cognome` )
+let colorePreferito = prompt( `inserisci il tuo colore preferito` )
+alert ( `La password generata per te è: 
+${nome}-${cognome}-${colorePreferito}-${annoCorrente}  ` )
